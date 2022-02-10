d3362d4ea1376fef81203a244ade65044043c473
Kritika Naagar <kritikanaagar15@gmail.com>
2022-02-10 22:48:16 +0530

    ejs setup

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
+
 app.get('/', (req, res) => {
-    res.render('index')
+    const articles = [
+        {
+            head: "Test",
+            createdAt: Date.now(),
+            description: "this is a test"
+        }
+    ]
+    res.render('index', { articles : articles})
 })
 
+// start at 14:28 
 app.listen(3000)
