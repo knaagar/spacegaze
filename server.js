972f9e948b6119ee0f11f98e6fe73916971d1629
Kritika Naagar <kritikanaagar15@gmail.com>
2022-02-13 22:28:48 +0530

    backend done

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb+srv://gitleaf:gitleaf112@cluster0.uxjun.mongodb.net/test?retryWrites=true&w=majority')
 
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    const articles = [
-        {
-            head: "Test",
-            createdAt: new Date(),
-            description: "this is a test"
-        }
-    ]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
     res.render('articles/index', { articles : articles})
 })
 
 // start at 27:36
+app.use('/articles', articleRouter)
+
 app.listen(3000)
